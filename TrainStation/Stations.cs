8a662a6ebd0f85cf5fd41ffec560a9204152144f
SpaceCowbooy <alex77539@gmail.com>
2023-03-09 01:38:36 +0300

    Доработал модель, начал делать заливку

--- TrainStation/Stations.cs
+++ TrainStation/Stations.cs
@@ -17,23 +17,46 @@ namespace TrainStation
     {
         public Point firstPoint;
         public Point secondPoint;
-        public double Length;
+        public double length;
+        public int index;
 
-        public StationLine(Point firstPoint, Point secondPoint, double length) {
+        public StationLine(int index, Point firstPoint, Point secondPoint, double length) {
             this.firstPoint = firstPoint;
             this.secondPoint = secondPoint;
-            Length = length;
+            this.length = length;
+            this.index = index;
         }
         public StationLine(double x1, double y1, double x2, double y2, double length) {
             firstPoint = new Point(x1, y1);
             secondPoint = new Point(x2, y2);
-            Length = length;
+            this.length = length;
+        }
+    }
+
+    public class StataionWay {
+        public List<StationLine> lines = new List<StationLine>();
+        public StataionWay(List<StationLine> lines) {
+            this.lines.AddRange(lines);
+        }
+    }
+
+    public class StataionPark
+    {
+        public string name;
+        public List<StataionWay> linesOfWay = new List<StataionWay>();
+        public StataionPark(string name, List<StataionWay> ways) {
+            this.name = name;
+            linesOfWay.AddRange(ways);
         }
     }
 
     public static class StationStructure {
-        public static List<StationLine> GetStations() {
-            var a = new Dictionary<int, Point> {
+        public static List<StationLine> Lines = new List<StationLine>();
+        public static List<StataionPark> Parks = new List<StataionPark>();
+        public static void CreateStructure() {
+
+            // Список всех точек для удобство заполнения
+            var p = new Dictionary<int, Point> {
                 { 1, new Point(160, 120) },
                 { 2, new Point(200, 80) },
                 { 3, new Point(240, 120) },
@@ -46,44 +69,62 @@ namespace TrainStation
                 { 10, new Point(320, 160) },
                 { 11, new Point(200, 200) }
             };
-            return new List<StationLine> {
-                new StationLine(a[1], a[2], 30),
-                new StationLine(a[1], a[3], 40),
-                new StationLine(a[1], a[5], 15),
 
-                new StationLine(a[2], a[1], 30),
-                new StationLine(a[2], a[7], 55),
+            Lines = new List<StationLine> {
+                new StationLine(1, p[1], p[2], 30),
+                new StationLine(2, p[1], p[3], 40),
+                new StationLine(3, p[1], p[5], 15),
 
-                new StationLine(a[3], a[1], 40),
-                new StationLine(a[3], a[4], 25),
-                new StationLine(a[3], a[5], 20),
-                new StationLine(a[3], a[7], 80),
+                new StationLine(4, p[2], p[7], 55),
 
-                new StationLine(a[4], a[3], 25),
-                new StationLine(a[4], a[6], 40),
-                new StationLine(a[4], a[8], 20),
-                new StationLine(a[4], a[9], 100),
+                new StationLine(5, p[3], p[4], 25),
+                new StationLine(6, p[3], p[5], 20),
+                new StationLine(7, p[3], p[7], 80),
 
-                new StationLine(a[5], a[1], 15),
-                new StationLine(a[5], a[3], 20),
-                new StationLine(a[5], a[6], 10),
+                new StationLine(8, p[4], p[6], 40),
+                new StationLine(9, p[4], p[8], 20),
+                new StationLine(10, p[4], p[9], 100),
 
-                new StationLine(a[6], a[4], 40),
-                new StationLine(a[6], a[5], 10),
-                new StationLine(a[6], a[10], 70),
+                new StationLine(11, p[5], p[6], 10),
 
-                new StationLine(a[7], a[2], 55),
-                new StationLine(a[7], a[3], 80),
-                new StationLine(a[7], a[8], 40),
+                new StationLine(12, p[6], p[10], 70),
+                new StationLine(13, p[6], p[11], 70),
 
-                new StationLine(a[8], a[4], 20),
-                new StationLine(a[8], a[7], 40),
+                new StationLine(14, p[7], p[8], 40),
+            };
+            
+            var l = new Dictionary<int, StationLine> {
+                { 1, new StationLine(1, p[1], p[2], 30) },
+                { 2, new StationLine(2, p[1], p[3], 40) },
+                { 3, new StationLine(3, p[1], p[5], 15) },
+                { 4, new StationLine(4, p[2], p[7], 55) },
 
-                new StationLine(a[9], a[4], 100),
+                { 5, new StationLine(5, p[3], p[4], 25) },
+                { 6, new StationLine(6, p[3], p[5], 20) },
+                { 7, new StationLine(7, p[3], p[7], 80) },
 
-                new StationLine(a[10], a[6], 70),
+                { 8, new StationLine(8, p[4], p[6], 40) },
+                { 9, new StationLine(9, p[4], p[8], 20) },
+                { 10, new StationLine(10, p[4], p[9], 100) },
 
-                new StationLine(a[11], a[6], 50),
+                { 11, new StationLine(11, p[5], p[6], 10) },
+
+                { 12, new StationLine(12, p[6], p[10], 70) },
+                { 13, new StationLine(13, p[6], p[11], 70) },
+
+                { 14, new StationLine(14, p[7], p[8], 40) }
+            };
+
+            var ways = new Dictionary<int, StataionWay> {
+                {1,  new StataionWay(new List<StationLine> {l[2], l[5], l[10] }) },
+                {2,  new StataionWay(new List<StationLine> {l[4], l[7], l[6], l[3] }) },
+                {3,  new StataionWay(new List<StationLine> {l[13], l[12]}) },
+                {4,  new StataionWay(new List<StationLine> {l[8], l[9], l[14] }) },
+            };
+
+            Parks = new List<StataionPark> {
+                { new StataionPark ("Красивый", new List<StataionWay> {ways[1], ways[2] }) },
+                { new StataionPark ("Сочный", new List<StataionWay> {ways[3], ways[4] }) },
             };
         }
     }
--- TrainStation/TrainStationViewModel.cs
+++ TrainStation/TrainStationViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -8,27 +9,48 @@ namespace TrainStation
 {
     internal class TrainStationViewModel : ObservableObject
     {
+        private ImageSource imageSource;
+        public ImageSource ImageSource {
+            get => imageSource;
+            set => SetProperty(ref imageSource, value);
+        }
+
+        private ObservableCollection<StataionPark> parks;
+
+        public ObservableCollection<StataionPark> Parks {
+            get => parks;
+            set => SetProperty(ref parks, value);
+        }
+
+        private StataionPark selectedPark;
+
+        public StataionPark SelectedPark {
+            get => selectedPark;
+            set => SetProperty(ref selectedPark, value);
+        }
+
         public TrainStationViewModel() {
 
-            TestCommand = new RelayCommand(Test);
-            var stations = StationStructure.GetStations();
-            var shapeOutlinePen = new Pen(Brushes.Black, 0.1);
+            //TestCommand = new RelayCommand(Test);
+            StationStructure.CreateStructure();
+            var stations = StationStructure.Lines;
+            Parks = new ObservableCollection<StataionPark>(StationStructure.Parks);
+            var shapeOutlinePen = new Pen(Brushes.Black, 0.5);
             var dGroup = new DrawingGroup();
             using (var dc = dGroup.Open()) {
                 foreach (var station in stations) {
-                    dc.DrawLine(shapeOutlinePen,station.firstPoint, station.secondPoint);
+                    dc.DrawLine(shapeOutlinePen, station.firstPoint, station.secondPoint);
+
                 }
             }
 
             var dImageSource = new DrawingImage(dGroup);
-            Source = dImageSource;
-        }
-        private ImageSource source;
-        public ImageSource Source {
-            get => source;
-            set => SetProperty(ref source, value);
+            ImageSource = dImageSource;
         }
 
+        public void FillPark (int index) {
+
+        }
         public ICommand TestCommand { get; }
         private void Test() => MessageBox.Show("123");
     }
